5f7d91fa1c78a7428a12ded8917b66037b11896f
oilindia <oil.india.mail@example.com>
2026-07-08 15:52:01 +0530

    20260708     1540

--- OIL/OIL.Web.Client/wwwroot/js/chart-functions.js
+++ OIL/OIL.Web.Client/wwwroot/js/chart-functions.js
@@ -1,4 +1,19 @@
-﻿window.downloadFileFromStreamStore = async (fileName, contentStream) => {
+﻿
+window.downloadFileFromStreamTemplate = async (fileName, contentStreamReference) => {
+    const arrayBuffer = await contentStreamReference.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+    anchorElement.download = fileName ?? '';
+    anchorElement.click();
+    anchorElement.remove();
+    URL.revokeObjectURL(url);
+};
+
+
+
+window.downloadFileFromStreamStore = async (fileName, contentStream) => {
     const arrayBuffer = await contentStream.arrayBuffer();
     const blob = new Blob([arrayBuffer]);
     const url = URL.createObjectURL(blob);
